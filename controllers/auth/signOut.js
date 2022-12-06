ab2bb795735253683da2d06cf52ef515477e0e92
Yevhen Podoliaka <93725802+yevhenpodoliaka@users.noreply.github.com>
2022-12-06 12:18:28 +0200

    fixed logout

--- controllers/auth/signOut.js
+++ controllers/auth/signOut.js
@@ -3,5 +3,5 @@ import { User } from "../../models/user.js";
 export const signOut = async (req, res) => {
      const { _id } = req.user;
      await User.findByIdAndUpdate(_id, { token: null });
-     res.status(204).json({ code: 204 });
+     res.status(204).send();
 }
